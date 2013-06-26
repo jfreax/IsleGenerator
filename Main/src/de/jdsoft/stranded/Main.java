5ff24a76f8c73c02d703cd40c2cd75feaeb314ad
Jens Dieskau <jfreax@googlemail.com>
2013-06-27 00:09:01 +0200

    Cleanup

--- Main/src/de/jdsoft/stranded/Main.java
+++ Main/src/de/jdsoft/stranded/Main.java
@@ -3,54 +3,27 @@ package de.jdsoft.stranded;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.graphics.g3d.ModelBatch;
-import com.badlogic.gdx.graphics.g3d.ModelInstance;
-import com.badlogic.gdx.graphics.g3d.lights.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.lights.Lights;
-import com.badlogic.gdx.graphics.g3d.materials.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.materials.Material;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.GdxRuntimeException;
-import com.marcrh.graph.Point;
-import de.jdsoft.stranded.Entity.Tile;
 import de.jdsoft.stranded.Map.TileManager;
 
 
 public class Main extends Game {
-    Matrix4 matrix = new Matrix4();
-
-
     TileManager tileManager;
 
-    //OrthographicCamera camera;
     PerspectiveCamera cam;
-    Model model;
-    ModelInstance instance;
-    ModelBatch modelBatch;
-
     CameraInputController camController;
 
     private Mesh mesh;
     private ShaderProgram shaderProgram;
-
-
-    ShapeRenderer shapeRenderer;
 
     Lights lights;
 
 
     @Override
     public void create() {
-        //camera = new OrthographicCamera();
-        //camera.setToOrtho(false, 800, 480);
-
         //check we can use GLES2
         if (!Gdx.app.getGraphics().isGL20Available()) {
             throw new GdxRuntimeException("GLES2 Not Available!");
@@ -81,12 +54,13 @@ public class Main extends Game {
             System.exit(0);
         }
 
+/*
 
         lights = new Lights();
         lights.ambientLight.set(0.4f, 0.4f, 0.4f, 1f);
         lights.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+*/
 
-        modelBatch = new ModelBatch();
 
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         cam.position.set(10f, 10f, 10f);
@@ -98,20 +72,12 @@ public class Main extends Game {
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);
 
-        ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(5f, 5f, 5f,
-                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-        instance = new ModelInstance(model);
-
         Gdx.graphics.getGL20().glEnable(GL20.GL_TEXTURE_2D);
         Gdx.graphics.getGL20().glEnable(GL20.GL_BLEND);
         Gdx.graphics.getGL20().glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
 
         tileManager = new TileManager(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
@@ -119,22 +85,9 @@ public class Main extends Game {
         camController.update();
         cam.update();
 
-
         Gdx.graphics.getGL20().glClearColor(0.2f, 0.2f, 0.2f, 1);
         Gdx.graphics.getGL20().glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-//        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-
-//        mesh.render(GL20.GL_TRIANGLE_STRIP);
-
-//        for( Tile tile : tileManager.getTiles()) {
-//            System.out.println( ""+ tile.getVertices().length);
-//            shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-//            shapeRenderer.setColor( new Color(tile.getColor(tileManager)) );
-//            shapeRenderer.polygon(tile.getVertices());
-//            shapeRenderer.end();
-//        }
 
         mesh = new Mesh(true, 3, 3,
                 new VertexAttribute(VertexAttributes.Usage.Position, 3, "a_position"));
@@ -147,21 +100,14 @@ public class Main extends Game {
 
         shaderProgram.begin();
         shaderProgram.setUniformMatrix("u_worldView", cam.combined);
-        //shaderProgram.setUniformi("u_texture", 0);
         mesh.render(shaderProgram, GL20.GL_TRIANGLES);
         shaderProgram.end();
 
-
-
-//        modelBatch.begin(cam);
-//        modelBatch.render(instance, lights);
-//        modelBatch.end();
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        model.dispose();
     }
 
     @Override

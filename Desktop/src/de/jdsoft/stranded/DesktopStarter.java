8dea0141da75056ed6fd3ad5e0fb2ee9066a9bc6
Jens Dieskau <jfreax@googlemail.com>
2013-06-26 22:38:03 +0200

    libgdx files

---
+++ Desktop/src/de/jdsoft/stranded/DesktopStarter.java
@@ -0,0 +1,16 @@
+package de.jdsoft.stranded;
+
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopStarter {
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Title";
+        cfg.useGL20 = true;
+        cfg.width = 480;
+        cfg.height = 800;
+        new LwjglApplication(new Main(), cfg);
+    }
+}
---
+++ Main/src/de/jdsoft/stranded/Main.java
@@ -0,0 +1,10 @@
+package de.jdsoft.stranded;
+
+import com.badlogic.gdx.Game;
+
+
+public class Main extends Game {
+    @Override
+    public void create() {
+    }
+}

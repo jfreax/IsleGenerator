7e59595e19ad41d777b5ee4d41b74791021076b3
Jens Dieskau <jfreax@googlemail.com>
2013-06-29 19:28:55 +0200

    Rotate and test code to use renderables

--- Main/src/de/jdsoft/stranded/Screens/DrawAsTexture.java
+++ Main/src/de/jdsoft/stranded/Screens/DrawAsTexture.java
@@ -4,16 +4,18 @@ package de.jdsoft.stranded.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.graphics.g3d.ModelBatch;
-import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.lights.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.lights.Lights;
 import com.badlogic.gdx.graphics.g3d.materials.Material;
 import com.badlogic.gdx.graphics.g3d.materials.TextureAttribute;
-import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.model.NodePart;
+import com.badlogic.gdx.graphics.g3d.shaders.BaseShader;
+import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
+import com.badlogic.gdx.graphics.g3d.utils.*;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.marcrh.graph.Point;
 import de.jdsoft.stranded.Entity.Tile;
 import de.jdsoft.stranded.Map.TileManager;
@@ -39,13 +41,17 @@ public class DrawAsTexture implements Screen {
     public ModelInstance instance;
 
     ShaderProgram shader;
+    private TestShader testShader;
+    private Renderable planet;
+    private RenderContext renderContext;
+    private DefaultShader shader2;
 
 
     @Override
     public void show() {
         // Set camera
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(10f, 10f, 10f);
+        cam.position.set(3f, 0f, 20f);
         cam.lookAt(0,0,0);
         cam.near = 0.1f;
         cam.far = 300f;
@@ -77,17 +83,40 @@ public class DrawAsTexture implements Screen {
         computeTexture();
 
 
+        // Test light source
         lights = new Lights();
         lights.ambientLight.set(0.4f, 0.4f, 0.4f, 1f);
         lights.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
+
+        // Create planet mesh
         modelBatch = new ModelBatch(Gdx.files.internal("shader/default.vertex.glsl"), Gdx.files.internal("shader/default.fragment.glsl"));
         ModelBuilder modelBuilder = new ModelBuilder();
         model = modelBuilder.createSphere(10.f, 10.f, 10.f, 50, 50,
                 new Material(TextureAttribute.createDiffuse(texture)),
                 VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
 
+
+        NodePart blockPart = model.nodes.get(0).parts.get(0);
+
+        planet = new Renderable();
+        planet.mesh = blockPart.meshPart.mesh;
+        planet.meshPartOffset = blockPart.meshPart.indexOffset;
+        planet.meshPartSize = blockPart.meshPart.numVertices;
+        planet.primitiveType = blockPart.meshPart.primitiveType;
+        planet.material = blockPart.material;
+        planet.lights = lights;
+        planet.worldTransform.idt();
+
+        renderContext = new RenderContext(new DefaultTextureBinder(DefaultTextureBinder.WEIGHTED, 1));
+        shader2 = new DefaultShader(
+                planet.material,
+                planet.mesh.getVertexAttributes(),
+                true, false, 1, 0, 0, 0);
+        shader2.init();
+
         instance = new ModelInstance(model);
+
 
     }
 
@@ -100,14 +129,24 @@ public class DrawAsTexture implements Screen {
         angle += delta*30;
         angle %= 360;
 
+
         Gdx.graphics.getGL20().glClearColor(0.2f, 0.2f, 0.2f, 0.4f);
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
-
+        instance.transform.rotate(new Vector3(0, 1, 0), delta*10);
         modelBatch.begin(cam);
         modelBatch.render(instance, lights);
-
         modelBatch.end();
+
+
+//        planet.worldTransform.rotate(new Vector3(0, 1, 0), delta*10);
+//
+//        renderContext.begin();
+//        shader2.begin(cam, renderContext);
+//        shader2.set((new BaseShader.Input(BaseShader.GLOBAL_UNIFORM, "time")), 30.0f);
+//        shader2.render(planet);
+//        shader2.end();
+//        renderContext.end();
     }
 
     void SetupShader() {
@@ -179,5 +218,69 @@ public class DrawAsTexture implements Screen {
     @Override
     public void dispose() {
     }
+
+    public static class MyModelBatch extends ModelBatch {
+        ShaderProvider getShaderProvider() {
+            return shaderProvider;
+        }
+    }
+
+
+    public static class TestShader extends BaseShader {
+        protected final BaseShader.Input u_projTrans	= register(new Input(GLOBAL_UNIFORM, "u_projTrans"));
+        protected final Input u_worldTrans	= register(new Input(GLOBAL_UNIFORM, "u_worldTrans"));
+        protected final Input u_test			= register(new Input(GLOBAL_UNIFORM, "u_test"));
+
+        protected final ShaderProgram program;
+
+        public TestShader () {
+            super();
+            program = new ShaderProgram(
+                    Gdx.files.internal("shader/default.vertex.glsl").readString(),
+                    Gdx.files.internal("shader/default.fragment.glsl").readString());
+            //program = new ShaderProgram(vertexShader, fragmentShader);
+            if (!program.isCompiled())
+                throw new GdxRuntimeException("Couldn't compile shader " + program.getLog());
+        }
+
+        @Override
+        public void init () {
+            super.init(program, 0, 0, 0);
+        }
+
+        @Override
+        public int compareTo (Shader other) {
+            return 0;
+        }
+
+        @Override
+        public boolean canRender (Renderable instance) {
+            return true;
+        }
+
+        @Override
+        public void begin (Camera camera, RenderContext context) {
+            program.begin();
+            set(u_projTrans, camera.combined);
+        }
+
+        @Override
+        public void render (Renderable renderable) {
+            set(u_worldTrans, renderable.worldTransform);
+            renderable.mesh.render(program, renderable.primitiveType, renderable.meshPartOffset, renderable.meshPartSize);
+        }
+
+        @Override
+        public void end () {
+            program.end();
+        }
+
+        @Override
+        public void dispose () {
+            super.dispose();
+            program.dispose();
+        }
+    }
+
 
 }

68022bc1a18e58eb10380ee85fe3c8aaf50cf9c6
Jens Dieskau <jfreax@googlemail.com>
2013-06-30 19:37:59 +0200

    Add world transformation

--- Android/assets/shader/planet_effect.fragment.glsl
+++ Android/assets/shader/planet_effect.fragment.glsl
@@ -3,7 +3,6 @@
 #endif
 
 uniform float u_time;
-uniform vec2 resolution;
 uniform sampler2D u_texture;
 varying vec4 v_color;
 
@@ -51,25 +50,9 @@ void main() {
     vec4 color = texture2D(u_texture, v_texCoords); // * v_color
 
     vec2 texPosition = (v_texCoords * 2.0f) - 1.0f;
-    //texPosition.x += time;
 
-    float x = gl_FragCoord.x/resolution.x;
-    float y = gl_FragCoord.y/resolution.y;
-    float z = gl_FragCoord.z; // Already in range [0,1]
-
-    // Converting from range [0,1] to NDC [-1,1]
-    float ndcx = x * 2.0 - 1.0;
-    float ndcy = y * 2.0 - 1.0;
-    float ndcz = z * 2.0 - 1.0;
-    vec3 ndc = vec3(ndcx, ndcy, ndcz);
 
     float len = length(texPosition);
-    //float len = length( ndc );
-    //color *= smoothstep(0.5, 0.495, len);
-
-    //vec2 texPosition = (v_texCoords * 2.0f) - 1.0f;
-    //vec2 texCoord = v_texCoords;
-    //texCoord += vec2(time); // / 360.f);
 
     color.a = 0.5f;
     color.rgb *= 1.0 - step(0.9, len);
@@ -87,13 +70,12 @@ void main() {
 
 
     float t = pow(field(p), .2);
-    color = vec4( t/3.f, t/2.f, t, t);
+    color = vec4( t/4.f, t/3.f, t/2.f, t/2.f);
 
     //color *= vec4( vec3( smoothstep(r, r+softness, len) ), 1.0f );
     color *= vec4( vec3( smoothstep(r, r+0.02f, len) ), 1.0f );
     color *= vec4( vec3( 1.0f - smoothstep(r+circleDiameter, r+circleDiameter+softness, len) ), 1.0f );
     //color *= vec4( vec3( 1.0f - smoothstep(r-softness, r+softness, len) ), 1.0f );
 
-    //color.a = 1.0f;
     gl_FragColor = color;
 }
--- Android/assets/shader/planet_effect.vertex.glsl
+++ Android/assets/shader/planet_effect.vertex.glsl
@@ -1,8 +1,11 @@
 attribute vec4 a_position;
+uniform mat4 u_worldTrans;
+uniform mat4 u_projTrans;
+
+
 attribute vec4 a_color;
 attribute vec2 a_texCoord0;
 
-uniform mat4 u_projTrans;
 uniform float u_time;
 
 varying vec4 v_color;
@@ -13,5 +16,5 @@ void main() {
 
     v_texCoords = a_texCoord0;
     //gl_Position = vec4(0);
-    gl_Position = (u_projTrans * a_position);
+    gl_Position = (u_projTrans * u_worldTrans * a_position);
 }
